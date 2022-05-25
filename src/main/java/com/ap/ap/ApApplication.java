21f23add4524ae87fc46e7cfefd009ff8611897c
Candela <caianeil27@gmail.com>
2022-05-24 23:07:24 -0300

    implementación de login

--- src/main/java/com/ap/ap/ApApplication.java
+++ src/main/java/com/ap/ap/ApApplication.java
@@ -1,13 +1,21 @@
 package com.ap.ap;
 
+import java.util.Arrays;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
 
-import java.util.Arrays;
+import com.ap.ap.security.JWTAuthorizationFilter;
 
 @SpringBootApplication
 public class ApApplication {
@@ -28,6 +36,21 @@ public class ApApplication {
 		UrlBasedCorsConfigurationSource urlBasedCorsConfigurationSource = new UrlBasedCorsConfigurationSource();
 		urlBasedCorsConfigurationSource.registerCorsConfiguration("/**", corsConfiguration);
 		return new CorsFilter(urlBasedCorsConfigurationSource);
+	}
+
+	@EnableWebSecurity
+	@Configuration
+	class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+		@Override
+		protected void configure(HttpSecurity http) throws Exception {
+			http.csrf().disable()
+					.addFilterAfter(new JWTAuthorizationFilter(), UsernamePasswordAuthenticationFilter.class)
+					.authorizeRequests()
+					.antMatchers(HttpMethod.POST, "/login").permitAll()
+					.antMatchers(HttpMethod.GET).permitAll()
+					.anyRequest().authenticated();
+		}
 	}
 
 }
--- src/main/java/com/ap/ap/models/Usuario.java
+++ src/main/java/com/ap/ap/models/Usuario.java
@@ -1,8 +1,15 @@
 package com.ap.ap.models;
 
-import javax.persistence.*;
 import java.io.Serializable;
 import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
 
 @Entity
 public class Usuario implements Serializable {
@@ -16,6 +23,9 @@ public class Usuario implements Serializable {
     private String titulo;
     private String descripcion;
     private String fotoPerfil;
+    private String email;
+    private String password;
+    private String token;
 
     @OneToMany(fetch = FetchType.LAZY, mappedBy = "idEdu")
     private List<Educacion> educacionList;
@@ -27,13 +37,15 @@ public class Usuario implements Serializable {
     public Usuario() {
     }
 
-    public Usuario(java.lang.Long id, java.lang.String nombre, java.lang.String apellido, java.lang.String titulo, java.lang.String descripcion, java.lang.String fotoPerfil) {
+    public Usuario(Long id, String nombre, String apellido, String titulo, String descripcion, String fotoPerfil, String email, String password) {
         this.id = id;
         this.nombre = nombre;
         this.apellido = apellido;
         this.titulo = titulo;
         this.descripcion = descripcion;
         this.fotoPerfil = fotoPerfil;
+        this.email = email;
+        this.password = password;
     }
 
     public java.lang.Long getId() {
@@ -82,5 +94,25 @@ public class Usuario implements Serializable {
 
     public void setFotoPerfil(java.lang.String fotoPerfil) {
         this.fotoPerfil = fotoPerfil;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
     }
 }
